be4e0abf312bb62403ac1c42174cf983c7f40a28
Christian Salthe <c0salt02@louisville.edu>
2018-05-03 16:41:16 -0400

    Commit

---
+++ Lab 6/Lab 6/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Lab_6
+{
+    partial class wpmGrader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wpmTextBox = new System.Windows.Forms.TextBox();
+            this.wpmLabel = new System.Windows.Forms.Label();
+            this.wpmButton = new System.Windows.Forms.Button();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.gradeOutPut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wpmTextBox
+            // 
+            this.wpmTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.wpmTextBox.Location = new System.Drawing.Point(200, 85);
+            this.wpmTextBox.Name = "wpmTextBox";
+            this.wpmTextBox.Size = new System.Drawing.Size(100, 20);
+            this.wpmTextBox.TabIndex = 0;
+            // 
+            // wpmLabel
+            // 
+            this.wpmLabel.AutoSize = true;
+            this.wpmLabel.BackColor = System.Drawing.SystemColors.Info;
+            this.wpmLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.wpmLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.wpmLabel.Location = new System.Drawing.Point(87, 86);
+            this.wpmLabel.Name = "wpmLabel";
+            this.wpmLabel.Size = new System.Drawing.Size(107, 15);
+            this.wpmLabel.TabIndex = 1;
+            this.wpmLabel.Text = "Words per minute:";
+            // 
+            // wpmButton
+            // 
+            this.wpmButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.wpmButton.Location = new System.Drawing.Point(200, 175);
+            this.wpmButton.Name = "wpmButton";
+            this.wpmButton.Size = new System.Drawing.Size(100, 23);
+            this.wpmButton.TabIndex = 2;
+            this.wpmButton.Text = "Calculate Grade";
+            this.wpmButton.UseVisualStyleBackColor = true;
+            this.wpmButton.Click += new System.EventHandler(this.wpmButton_Click);
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.gradeLabel.Location = new System.Drawing.Point(98, 280);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(96, 15);
+            this.gradeLabel.TabIndex = 4;
+            this.gradeLabel.Text = "Grade recieved: ";
+            // 
+            // gradeOutPut
+            // 
+            this.gradeOutPut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gradeOutPut.Location = new System.Drawing.Point(200, 281);
+            this.gradeOutPut.Name = "gradeOutPut";
+            this.gradeOutPut.Size = new System.Drawing.Size(100, 20);
+            this.gradeOutPut.TabIndex = 5;
+            // 
+            // wpmGrader
+            // 
+            this.AcceptButton = this.wpmButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(473, 355);
+            this.Controls.Add(this.gradeOutPut);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.wpmButton);
+            this.Controls.Add(this.wpmLabel);
+            this.Controls.Add(this.wpmTextBox);
+            this.Name = "wpmGrader";
+            this.Text = "Words Per Minute Grader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox wpmTextBox;
+        private System.Windows.Forms.Label wpmLabel;
+        private System.Windows.Forms.Button wpmButton;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.Label gradeOutPut;
+    }
+}
+
---
+++ Lab 6/Lab 6/Form1.cs
@@ -0,0 +1,67 @@
+﻿// X1084
+// Lab 6 
+// 3/25/2018
+// CIS 199-02
+// This program takes a user input word per minute score and outputs a letter grade associated to the input typing score, using parrallel array range matching. 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6
+{
+    public partial class wpmGrader : Form
+    {
+        public wpmGrader()
+        {
+            InitializeComponent();
+        }
+        // this event handeler executes the below code upon the clicking of the wpmButton 
+        private void wpmButton_Click(object sender, EventArgs e)
+        {
+      
+
+            int[] words = new int[] { 76, 51, 31, 16, 0 }; // This array holds the minimum values associated with each grade letter range  
+            char[] grades = new char[] { 'A', 'B', 'C', 'D', 'F' }; // THis array holds the letter grade values associated to typing score ranges in the parrallel words array 
+
+            int wpm; // This is the users input score on the typing test
+            char grade = 'F'; // This is the associated letter grade derived from the user input wpm score 
+            bool found = false;// This determines if the user input wpm score fits a given test score range 
+            
+
+
+
+            if (int.TryParse(wpmTextBox.Text, out wpm) && (wpm >= 0)) // Try parse the user input wpm score 
+            {
+                for (int i = 0; i < words.Length && !found; ++i) // Iterates through words loop to match user input value with pre set ranges 
+                {
+                    if (wpm >= words[i]) // IF statement to test if user input value matches pre set ranges 
+                    {
+                        found = true;
+                        grade = grades[i];
+                    }
+
+                }
+                // output
+                gradeOutPut.Text = ($"{grade}");
+
+            }
+            else
+            {
+                //invalid user input 
+                MessageBox.Show("Enter valid wpm");
+            }
+         
+
+
+
+
+
+            }
+    }
+}
